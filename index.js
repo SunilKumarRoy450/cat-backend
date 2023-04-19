8077229dc89df84573acf1ec5ce0f4f2230fd77d
SunilKumarRoy450 <101394814+SunilKumarRoy450@users.noreply.github.com>
2023-04-19 14:33:18 +0530

    chnages made

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ const app = express();
 const connection = require("./config/database");
 const PORT = 8080;
 const catRoute = require("./controllers/user.controller");
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
+
 app.use("/cats", catRoute);
+
 app.listen(PORT, async () => {
   try {
     await connection();
